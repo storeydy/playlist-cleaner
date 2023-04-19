79224961893f0e016925ad970c8fdc7413cbbd4f
Dylan Storey <storeydy@tcd.ie>
2023-04-19 21:45:30 +0100

    added some structure and added function for getting users playlists

---
+++ spotify-listening-trends/src/homepage/homepage.ts
@@ -0,0 +1,31 @@
+export async function fetchProfile(token: string): Promise<UserProfile> {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
+
+export async function getPlaylists(token: string, userId: string): Promise<any> {
+    const result = await fetch("https://api.spotify.com/v1/users/" + userId + "/playlists", {
+        method: "GET", headers: { Authorization: `Bearer ${token}`}
+    })
+    return await result.json();
+}
+
+export function populateUI(profile: UserProfile) {
+    document.getElementById("displayName")!.innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar")!.appendChild(profileImage);
+    }
+    document.getElementById("id")!.innerText = profile.id;
+    document.getElementById("email")!.innerText = profile.email;
+    document.getElementById("uri")!.innerText = profile.uri;
+    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url")!.innerText = profile.href;
+    document.getElementById("url")!.setAttribute("href", profile.href);
+    document.getElementById("followers")!.innerText = profile.followers.total.toString();
+    document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+}
